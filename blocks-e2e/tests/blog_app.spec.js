5b59010df99d1c697498203bcc7922f57fa8579b
Kimmo T <tuokkolk@gmail.com>
2024-06-18 21:23:35 +0300

    Fullstackopen - osa 5 - end to end -testaus: Playwright - tehtävät 5.17 - 5.20

--- blocks-e2e/tests/blog_app.spec.js
+++ blocks-e2e/tests/blog_app.spec.js
@@ -29,41 +29,39 @@ describe('Blog app', () => {
     await page.goto('http://localhost:5173')
   })
 
-  test('front page can be opened', async ({ page }) => {
-    const locator = await page.getByText('Blogit')
-    await expect(locator).toBeVisible()
+  test('Login form is shown', async ({ page }) => {
     await expect(page.getByText(/log in to application/i)).toBeVisible()
   })
 
-  test('user can login with correct credentials', async ({ page }) => {
-    await loginWith(page, userObj.username, userObj.password)
-    await expect(page.getByText(`${userObj.name} logged in`)).toBeVisible()
+  describe('Login', () => {
+    test('succeeds with correct credentials', async ({ page }) => {
+      await loginWith(page, userObj.username, userObj.password)
+      await expect(page.getByText(`${userObj.name} logged in`)).toBeVisible()
+    })
+
+    test('fails with wrong credentials', async ({ page }) => {
+      await loginWith(page, userObj.username, userObj.password + 'xxx')
+
+      const errorDiv = await page.locator('.error')
+      await expect(errorDiv).toContainText('wrong username or password')
+      await expect(errorDiv).toHaveCSS('color', 'rgb(255, 0, 0)')
+
+      await expect(page.getByText(`${userObj.name} logged in`)).not.toBeVisible()
+    })
   })
 
-  test('login fails with wrong password', async ({ page }) => {
-    await loginWith(page, userObj.username, userObj.password + 'xxx')
-
-    const errorDiv = await page.locator('.error')
-    await expect(errorDiv).toContainText('wrong username or password')
-    await expect(errorDiv).toHaveCSS('color', 'rgb(255, 0, 0)')
-
-    await expect(page.getByText(`${userObj.name} logged in`)).not.toBeVisible()
-  })
-
-  describe('when logged in', () => {
+  describe('When logged in', () => {
 
     beforeEach(async ({ page }) => {
       await loginWith(page, userObj.username, userObj.password)
-      //await page.waitForTimeout(3000)
     })
 
     test('a new blog can be created', async ({ page }) => {
       await createBlog(page, blogObj)
-      //await expect(page.locator(`p:has-text("${blogObj.title}"`).last()).toBeVisible()
       await expect(page.getByText(blogObj.title, { exact: false }).last()).toBeVisible()
     })
 
-    describe('and a blog exists', () => {
+    describe('When a blog exists', () => {
       beforeEach(async ({ page }) => {
         await createBlog(page, blogObj)
       })
@@ -71,8 +69,10 @@ describe('Blog app', () => {
       test('likes can be increased', async ({ page }) => {
         await page.getByRole('button', { name: /view/i }).click()
         await page.getByRole('button', { name: /like/i }).click()
+
         const expectedSubstr = '' + (Number(blogObj.votes) + 1)
-        await expect(page.getByTestId('blog_votes')).toContainText(expectedSubstr)
+        await expect(page.getByTestId('blog_votes'))
+          .toContainText(expectedSubstr)
       })
     })
   })
